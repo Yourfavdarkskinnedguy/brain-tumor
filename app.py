a11863c0fc531146c821050748597a77a20d68db
sholademi daniel <sholademidaniel@gmail.com>
2024-12-23 16:11:25 +0100

    made changes

--- app.py
+++ app.py
@@ -15,30 +15,34 @@ model = tf.keras.models.load_model(os.path.join(os.getcwd(),"BrainTumor.h5"))
 @app.route("/", methods=["GET", "POST"])
 def predict():
     if request.method == "POST":
-        # Check if a file is uploaded
-        if "file" not in request.files:
-            return render_template("home.html", result="No file uploaded!")
+        try:
+            print('Got here:', request)
+            # Check if a file is uploaded
+            if "file" not in request.files:
+                return render_template("home.html", result="No file uploaded!")
 
-        file = request.files["file"]
-        if file.filename == "":
-            return render_template("home.html", result="No selected file!")
-        
-        img = BytesIO(file.read())
+            file = request.files["file"]
+            if file.filename == "":
+                return render_template("home.html", result="No selected file!")
+            
+            img = BytesIO(file.read())
 
-        # Process the uploaded image
-        loaded_img = tf.keras.preprocessing.image.load_img(img, target_size=(128, 128))
-        img_to_array = tf.keras.preprocessing.image.img_to_array(loaded_img) / 255.0
-        img_expanded = np.expand_dims(img_to_array, axis=0)
+            # Process the uploaded image
+            loaded_img = tf.keras.preprocessing.image.load_img(img, target_size=(128, 128))
+            img_to_array = tf.keras.preprocessing.image.img_to_array(loaded_img) / 255.0
+            img_expanded = np.expand_dims(img_to_array, axis=0)
 
-        # Make prediction
-        predictions = model.predict(img_expanded)
-        print(predictions)  # For debugging purposes
-        result = "Healthy" if predictions[0][0] > 0.5 else "Tumor"
+            # Make prediction
+            predictions = model.predict(img_expanded)
+            print(predictions)  # For debugging purposes
+            result = "Healthy" if predictions[0][0] > 0.5 else "Tumor"
 
-        # Return result to the webpage
-        return render_template("home.html", result=result)
-
-    return render_template("home.html", result=None)
+            # Return result to the webpage
+            return render_template("home.html", result=result)
+        except:
+            print('Something went wrong')
+            render_template("home.html", result=None)
+        return render_template("home.html", result=None)
 
 
 # Main function to run the Flask app
