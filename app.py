a579ad49df9a3018e8f38e4cdc0a2e1b3889d89f
sholademi daniel <sholademidaniel@gmail.com>
2024-12-23 19:32:51 +0100

    changes

--- app.py
+++ app.py
@@ -40,7 +40,8 @@ def predict():
 
 
             # Process the uploaded image
-            # loaded_img = tf.keras.utils.load_img(full_path, target_size=(128, 128))
+            loaded_img = tf.keras.utils.load_img(full_path, target_size=(128, 128))
+            print(loaded_img)
             # img_to_array = tf.keras.utils.img_to_array(loaded_img) / 255.0
             # img_expanded = np.expand_dims(img_to_array, axis=0)
             # print(img_expanded)
