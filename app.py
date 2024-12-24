deed9f28bd39bc429ffbdcc8a03d7b63f90b1de5
sholademi daniel <sholademidaniel@gmail.com>
2024-12-24 10:02:07 +0100

    changes

--- app.py
+++ app.py
@@ -11,7 +11,10 @@ app = Flask(__name__)
 
 gpus= tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
+    tf.config.experimental.set_virtual_device_configuration(
+                gpu,
+                [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=512)]
+            )
 
 # Load the trained model
 model = tf.keras.models.load_model(os.path.join(os.getcwd(),"BrainTumor.h5"))
